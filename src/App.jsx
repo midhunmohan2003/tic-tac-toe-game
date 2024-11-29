df2b9dd5d403d2f16a7c8979d7f9b033d6f44d74
midhunmohan2003 <midhunmohan2215@gmail.com>
2024-11-29 13:36:23 +0530

    tic-tac-toe game

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css'
+import TicTacToe from './components/TicTacToe/TicTacToe'
+
+
+function App() {
+
+  return (
+    <>
+     <TicTacToe/>
+    </>
+  )
+}
+
+export default App
